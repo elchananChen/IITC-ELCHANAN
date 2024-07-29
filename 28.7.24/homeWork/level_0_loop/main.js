eb0bf742ab772579f138fd6138009955eaff2167
elchanan chen <elchananc79@gmail.com>
2024-07-30 00:52:52 +0300

    good night

--- 28.7.24/homeWork/level_0_loop/main.js
+++ 28.7.24/homeWork/level_0_loop/main.js
@@ -117,3 +117,133 @@
 // }
 
 // 18
+
+// let numbers = [1, 2, 3, 4, 5 , 6];
+
+// for (let i = numbers.length; i >= 0; i--) {
+//   let backward = numbers[i];
+//   console.log(backward);
+// }
+
+// 19
+
+// let arrayNumbers = [1, 2, 3, 4, 5, 6];
+
+// for (let i = 1; i < arrayNumbers.length; i += 2) {
+//   const printArrayNumbers = arrayNumbers[i];
+
+//   console.log(printArrayNumbers);
+// }
+
+// 20
+
+// let colorsArray = ["red", "green", "blue"];
+// for (let i = 0; i < colorsArray.length; i++) {
+//   let color = "color: [" + colorsArray[i] + "]";
+//   console.log(color);
+// }
+
+// 21
+
+// let sum = 0;
+// for (let i = 1; i < 6; i++) {
+//   sum = sum + i;
+//   console.log(sum);
+// }
+
+// 22
+
+// let product = 1;
+// for (let i = 1; i < 6; i++) {
+//   product = product * i;
+//   console.log(product);
+// }
+
+// 23
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8]; // 🤣לא מה שביקשו אבל יצא חמוד
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     count = count + arr[i];
+//     console.log(count);
+//   }
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     count = count + 1;
+//     console.log(count);
+//   }
+// }
+
+// 24
+
+// let arr = [10, 5, 8, 12, 3];
+// let largest = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (largest < arr[i]) {
+//     let largest = arr[i];
+//   }
+// }
+// console.log(largest);
+
+// 25
+
+// let count = 0;
+// for (let i = 1; i < 11; i++) {
+//   if (i % 2 === 0) {
+//     count = count + i;
+//     console.log(count);
+//   }
+// }
+
+// // 26
+
+// for (let i = 0; i < 2; i++) {
+//   console.log("```");
+//   if (i === 1) {
+//     break;
+//   }
+//   for (let j = 1; j < 4; j++) {
+//     console.log("*".repeat(j));
+//   }
+// }
+
+// 27
+
+// for (let i = 0; i < 2; i++) {
+//   console.log("***");
+//   if (i === 1) {
+//     break;
+//   }
+//   for (let j = 1; j < 2; j++) {
+//     console.log("***");
+//   }
+// }
+
+// 28
+
+for (let i = 0; i < 2; i++) {
+  console.log("```");
+  if (i === 1) {
+    break;
+  }
+  for (let j = 1; j < 4; j++) {
+    console.log("1".repeat(4));
+  }
+}
+
+// 30
+
+// for (let i = 0; i < 2; i++) {
+//   console.log("```");
+//   if (i === 1) {
+//     break;
+//   }
+//   for (let j = 1; j < 4; j++) {
+//     console.log(j.repeat(j));
+//   }
+// }
