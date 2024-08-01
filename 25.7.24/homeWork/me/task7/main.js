0a7df435a803afdb7d58e081a369d8cc2464e468
elchanan chen <elchananc79@gmail.com>
2024-08-02 00:31:31 +0300

    if_else_progres

--- 25.7.24/homeWork/me/task7/main.js
+++ 25.7.24/homeWork/me/task7/main.js
@@ -150,72 +150,168 @@
 
 // 14
 
-let age = 100;
-let discount = 4;
-if (age > 65 || age < 18) {
-  discount = 0.2;
-} else discount = 0;
-console.log(discount);
+// let age = 100;
+// let discount = 4;
+// if (age > 65 || age < 18) {
+//   discount = 0.2;
+// } else discount = 0;
+// console.log(discount);
 
-// // 15
+// // // 15
 
-let inRange;
-let number = 10;
-if (number >= 1 && number <= 10) {
-  inRange = true;
-} else inRange = false;
-// console.log(inRange);
+// let inRange;
+// let number = 10;
+// if (number >= 1 && number <= 10) {
+//   inRange = true;
+// } else inRange = false;
+// // console.log(inRange);
 
-// 16
+// // 16
 
-let dayNumber = 6;
-let dayName;
+// let dayNumber = 6;
+// let dayName;
 
-switch (dayNumber) {
-  case 1:
-    dayName = "sunday";
-    break;
-  case 2:
-    dayName = "monday";
-    break;
-  case 3:
-    dayName = "tuesday";
-    break;
-  case 4:
-    dayName = "wednesday";
-    break;
-  case 5:
-    dayName = "thutsday";
-    break;
-  case 6:
-    dayName = "friday";
-    break;
-  case 7:
-    dayName = "saturday";
-    break;
-  default:
-    dayName = "not a day";
-    break;
-}
-console.log(dayName);
+// switch (dayNumber) {
+//   case 1:
+//     dayName = "sunday";
+//     break;
+//   case 2:
+//     dayName = "monday";
+//     break;
+//   case 3:
+//     dayName = "tuesday";
+//     break;
+//   case 4:
+//     dayName = "wednesday";
+//     break;
+//   case 5:
+//     dayName = "thutsday";
+//     break;
+//   case 6:
+//     dayName = "friday";
+//     break;
+//   case 7:
+//     dayName = "saturday";
+//     break;
+//   default:
+//     dayName = "not a day";
+//     break;
+// }
+// console.log(dayName);
 
-// 17
+// // 17
+// let grade = "F";
+// let description;
+// switch (grade) {
+//   case "A":
+//     description = "exelent";
+//     break;
+//   case "B":
+//     description = "good";
+//     break;
+//   case "C":
+//     description = "not good";
+//     break;
+//   case "D":
+//     description = "descasting";
+//     break;
+//   case "F":
+//     description = "what????";
+//     break;
+// }
 
-//  ((dayNumber = 1)) {
-//   dayName = "Sunday";
-// } else if ((dayNumber = 2)) {
-//   dayName = "Monday";
-// } else if ((dayNumber = 3)) {
-//   dayName = "Tuesday";
-// } else if ((dayNumber = 4)) {
-//   dayName = "Wednesday";
-// } else if ((dayNumber = 5)) {
-//   dayName = "Thutsday";
-// } else if ((dayNumber = 6)) {
-//   dayName = "Friday";
-// } else if ((dayNumber = 7)) {
-//   dayName = "Saturday";
-// } else dayName = "not a day";
+// console.log(description);
+
+// 18
+
+// let sign;
+// let number = 3;
+// if (number > 0) {
+//   sign = "+";
+// } else if ((number = 0)) {
+//   sign = "0";
+// } else if (number < 0) {
+//   sign = "-";
+// } else sign = "not a number";
+
+// console.log(sign);
+
+// // 19
+
+// let year = 2400;
+// let isCenturyLeapYear;
+// if (year % 400 === 0) {
+//   isCenturyLeapYear = true;
+// } else isCenturyLeapYear = false;
+
+// console.log(isCenturyLeapYear);
+
+// // הביטוי הרגולרי
+// const pattern = /0{2}$/;
+
+// // רשימה של מחרוזות לבדיקה
+// const strings = [
+//   "hello world 1000",
+//   "hello world 100",
+//   "test 00",
+//   "end 000",
+//   "final 12300",
+// ];
+
+// // נבדוק כל מחרוזת
+// strings.forEach((s) => {
+//   if (pattern.test(s)) {
+//     console.log(`'${s}' מתאים לביטוי הרגולרי`);
+//   } else {
+//     console.log(`'${s}' אינו מתאים לביטוי הרגולרי`);
+//   }
+// });
+
+// 20
+
+// let month = 5;
+// let daysInMonth;
+// switch (month) {
+//   case 1:
+//     daysInMonth = 29;
+//     break;
+//   case 2:
+//     daysInMonth = 30;
+//     break;
+//   case 3:
+//     daysInMonth = 31;
+//     break;
+//   case 4:
+//     daysInMonth = 29;
+//     break;
+//   case 5:
+//     daysInMonth = 32;
+//     break;
+//   case 6:
+//     daysInMonth = 28;
+//     break;
+//   case 7:
+//     daysInMonth = 30;
+//     break;
+//   case 8:
+//     daysInMonth = 31;
+//     break;
+//   case 9:
+//     daysInMonth = 29;
+//     break;
+//   case 10:
+//     daysInMonth = 30;
+//     break;
+//   case 11:
+//     daysInMonth = 28;
+//     break;
+//   case 12:
+//     daysInMonth = 32;
+//     break;
+// }
+// console.log(daysInMonth);
+
+// 21
 
 // 31
 
