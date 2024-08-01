a392b807de2296378960df89232197133f386a80
elchanan chen <elchananc79@gmail.com>
2024-08-01 22:24:55 +0300

    rearguard_25.7_task7

--- 1.8.24/on class/nested_loops_task/main.js
+++ 1.8.24/on class/nested_loops_task/main.js
@@ -1,14 +1,43 @@
+"use strict";
+
 // 1
 
-//מגדיר משתנה
+// for (let i = 0; i < 2; i++) {
+//   let asterix = "";
+//   // asterix += "* * \n";
+//   for (let j = 0; j < 2; j++) {
+//     asterix += "*";
+//   }
+//   console.log(asterix);
+//   // asterix += "\n";
+// }
 
-for (let i = 0; i < 2; i++) {
-  let asterix = "";
-  // asterix += "* * \n";
-  for (let j = 0; j < 2; j++) {
-    asterix += "*";
-  }
-  // asterix += "\n";
-}
-console.log(3);
-console.log(asterix);
+// 2
+
+// function print123(num) {
+//   let count = 1;
+//   for (let i = 0; i < num; i++) {
+//     let row = "";
+//     for (let j = 0; j < num; j++) {
+//       row += count + " ";
+//       count++;
+//     }
+//     console.log(row + "");
+//   }
+// }
+// print123(2);
+
+// 3
+
+// function raTriangele(num) {
+//   for (let i = 0; i < num; i++) {
+//     let row = "";
+//     for (let j = 0; j <= i; j++) {
+//       row += "*";
+//     }
+//     console.log(row);
+//   }
+// }
+// raTriangele(8);
+
+// 4
---
+++ 25.7.24/homeWork/me/task7/main.js
@@ -0,0 +1,230 @@
+"use strict";
+// /*
+// // 1
+// let cenVote;
+// let age = 17;
+// if (age >= 18) {
+//   cenVote = true;
+// } else cenVote = false;
+// console.log(cenVote);
+
+// // 2
+
+// let temperature = 0;
+// let weather;
+// if (temperature < 0) {
+//   weather = "freezing";
+// } else weather = "not freezing";
+// console.log(weather);
+
+// // 3
+// let score = 60;
+// let result;
+// if (score >= 60) {
+//   result = "pass";
+// } else result = "fail";
+// console.log(result);
+
+// // 4
+
+// let grade;
+// let letterGrade;
+// if (grade >= 90) {
+//   grade = "A";
+// } else if (grade >= 80) {
+//   grade = "B";
+// } else grade = "C";
+
+// // 5
+
+// let number = 1;
+// let isEven;
+// if (number % 2 === 0) {
+//   isEven = true;
+// } else isEven = !true;
+
+// console.log(isEven);
+// */
+
+// // 6
+
+// let year = 32;
+// let isLeapYear;
+// if (year % 4 === 0) {
+//   isLeapYear = !false;
+// } else isLeapYear = false;
+// console.log(isLeapYear);
+
+// // 7
+
+// let hour = 4;
+// let period;
+// if (hour < 12) {
+//   period = "AM";
+// } else period = "PM";
+
+// // 8
+
+// let dayNumber;
+// let dayName;
+// if ((dayNumber = 1)) {
+//   dayName = "Sunday";
+// } else if ((dayNumber = 2)) {
+//   dayName = "Monday";
+// } else if ((dayNumber = 3)) {
+//   dayName = "Tuesday";
+// } else if ((dayNumber = 4)) {
+//   dayName = "Wednesday";
+// } else if ((dayNumber = 5)) {
+//   dayName = "Thutsday";
+// } else if ((dayNumber = 6)) {
+//   dayName = "Friday";
+// } else if ((dayNumber = 7)) {
+//   dayName = "Saturday";
+// } else dayName = "not a day";
+
+// // 9
+
+// let name;
+// if (name === undefined) {
+//   hasName = false;
+// } else hasName = true;
+// console.log(hasName);
+
+// // 10
+
+// let amount;
+// let shipping;
+// if (amount > 1000) {
+//   shipping = 0;
+// } else shipping = 5;
+
+// // 11
+
+// let passwrd;
+// let isLoggedIn;
+// if ((passwrd = "secret123")) {
+//   passwrd = true;
+// } else passwrd = false;
+
+// // 12
+
+// let month;
+// let season;
+// if ((nonth = 1)) {
+//   season = "winter";
+// } else if ((nonth = 2)) {
+//   season = "winter";
+// } else if ((nonth = 3)) {
+//   season = "winter";
+// } else if ((nonth = 4)) {
+//   season = "spring";
+// } else if ((nonth = 5)) {
+//   season = "summer";
+// } else if ((nonth = 6)) {
+//   season = "summer";
+// } else if ((nonth = 7)) {
+//   season = "summer";
+// } else if ((nonth = 8)) {
+//   season = "summer";
+// } else if ((nonth = 9)) {
+//   season = "fall";
+// } else if ((nonth = 10)) {
+//   season = "fall";
+// } else if ((nonth = 11)) {
+//   season = "winter";
+// } else if ((nonth = 12)) {
+//   season = "winter";
+// }
+
+// // 13
+
+// let loanApproved;
+// let income = 499999;
+// let creditScore = 8000;
+// if (income > 50000 && creditScore > 700) {
+//   loanApproved = true;
+// } else loanApproved = false;
+
+// console.log(loanApproved);
+
+// 14
+
+let age = 100;
+let discount = 4;
+if (age > 65 || age < 18) {
+  discount = 0.2;
+} else discount = 0;
+console.log(discount);
+
+// // 15
+
+let inRange;
+let number = 10;
+if (number >= 1 && number <= 10) {
+  inRange = true;
+} else inRange = false;
+// console.log(inRange);
+
+// 16
+
+let dayNumber = 6;
+let dayName;
+
+switch (dayNumber) {
+  case 1:
+    dayName = "sunday";
+    break;
+  case 2:
+    dayName = "monday";
+    break;
+  case 3:
+    dayName = "tuesday";
+    break;
+  case 4:
+    dayName = "wednesday";
+    break;
+  case 5:
+    dayName = "thutsday";
+    break;
+  case 6:
+    dayName = "friday";
+    break;
+  case 7:
+    dayName = "saturday";
+    break;
+  default:
+    dayName = "not a day";
+    break;
+}
+console.log(dayName);
+
+// 17
+
+//  ((dayNumber = 1)) {
+//   dayName = "Sunday";
+// } else if ((dayNumber = 2)) {
+//   dayName = "Monday";
+// } else if ((dayNumber = 3)) {
+//   dayName = "Tuesday";
+// } else if ((dayNumber = 4)) {
+//   dayName = "Wednesday";
+// } else if ((dayNumber = 5)) {
+//   dayName = "Thutsday";
+// } else if ((dayNumber = 6)) {
+//   dayName = "Friday";
+// } else if ((dayNumber = 7)) {
+//   dayName = "Saturday";
+// } else dayName = "not a day";
+
+// 31
+
+// let number = 3;
+// let parity = number % 2 === 0 ? "even" : "odd";
+// console.log(parity);
+
+// // 32
+
+// let age = 4;
+// let canVote = age >= 18 ? true : false;
+// console.log(canVote);
