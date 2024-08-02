8da72ba77a447eed2f9826d61c8d23badc04d316
elchanan chen <elchananc79@gmail.com>
2024-08-02 20:00:07 +0300

    shabessss

--- 25.7.24/homeWork/me/task9/main.js
+++ 25.7.24/homeWork/me/task9/main.js
@@ -16,3 +16,66 @@ function stringCharacter(str) {
 console.log(stringCharacter("128356456"));
 
 // 5
+
+function extractsStrongPortion(str) {
+  let portionString = str.substring(1, 7);
+  return portionString;
+}
+console.log(extractsStrongPortion("bananas"));
+
+// 6
+
+function replaceInString(str) {
+  let replace = str.replace("blue", "red");
+  return replace;
+}
+console.log(replaceInString("blue car"));
+
+// 7
+
+function removesWhitespace(str) {
+  let fixed = str.trim();
+  return fixed;
+}
+console.log(removesWhitespace("   dfs  dfsd  "));
+
+// 8
+
+function startWithHello(str) {
+  let check = str.startsWith("hello");
+  return check;
+}
+console.log(startWithHello("hello world"));
+
+// 9
+
+function ifEndWithDot(str) {
+  let check = str.endsWith(".");
+  return check;
+}
+console.log(ifEndWithDot("i am."));
+
+// 10
+
+function stringIndex(str) {
+  let index = str.indexOf(" ");
+  return index + 1;
+}
+console.log(stringIndex("hello baba"));
+
+// 11
+function splitStrig(str) {
+  let stringArray = str.split("o");
+  return stringArray;
+}
+console.log(splitStrig("for you "));
+
+// 12
+
+function repeatString(str) {
+  let repeat = str.repeat(3);
+  return repeat;
+}
+console.log(repeatString("why????  "));
+
+// 13
