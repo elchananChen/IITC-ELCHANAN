f538a56aa729c8384f3496ab8982a7642047c4f0
elchanan chen <elchananc79@gmail.com>
2024-08-02 15:06:48 +0300

    startOfStringMathods

---
+++ 25.7.24/homeWork/me/task9/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// 1
+
+function stringLength(str) {
+  return str.length;
+}
+console.log(stringLength("sd"));
+
+// 4
+
+function stringCharacter(str) {
+  return str.charAt(5);
+}
+
+console.log(stringCharacter("128356456"));
+
+// 5
