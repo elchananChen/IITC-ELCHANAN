b2b1c56b49a5761306b5038e155138f369dc25ef
elchanan chen <elchananc79@gmail.com>
2024-08-02 14:49:00 +0300

    25.7.function

--- 25.7.24/homeWork/me/task8/main.js
+++ 25.7.24/homeWork/me/task8/main.js
@@ -37,11 +37,11 @@ console.log(sumTwo(4, 9));
 
 // 8
 
-function getAbsoluteValue(num) {
-  if (num < 0) return num * -1;
-  else return num;
-}
-console.log(getAbsoluteValue(-3));
+// function getAbsoluteValue(num) {
+//   if (num < 0) return num * -1;
+//   else return num;
+// }
+// console.log(getAbsoluteValue(-3));
 
 // 9
 
@@ -58,3 +58,59 @@ function convertToUppetCase(str) {
 console.log(convertToUppetCase("rjdlkjf"));
 
 // 11
+
+function isPositive(num) {
+  if (num > 0) return true;
+  else return false;
+}
+console.log(isPositive(6));
+
+// 12
+
+function getFirstChar(str) {
+  return str[1];
+}
+console.log(getFirstChar("drtffd"));
+
+// 13
+
+function areaOfRectangle(height, width) {
+  return height * width;
+}
+
+// 15
+
+function logType(input) {
+  console.log(typeof input);
+}
+logType(3);
+
+// 16
+
+function invertBoolean(boolean) {
+  return !boolean;
+}
+console.log(invertBoolean(2 === 3));
+
+// 17
+
+function concatenateStrings(str, strTwo) {
+  return str + strTwo;
+}
+
+// 19
+
+function greetWithDefault(name, def) {
+  if (name === undefined) {
+    return `welcome ${name} you are beautiful`;
+  } else return (def = "guest");
+}
+
+// 20
+
+function isLongstring(str) {
+  if (str.length > 10) {
+    return true;
+  } else return false;
+}
+console.log(isLongstring("dlf342342352sd"));
