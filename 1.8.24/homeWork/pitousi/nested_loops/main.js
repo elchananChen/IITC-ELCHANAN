f3bae2a8916b965f54982da3701d07f6e644979a
elchanan chen <elchananc79@gmail.com>
2024-08-03 23:04:40 +0300

    nested loops and function

---
+++ 1.8.24/homeWork/pitousi/nested_loops/main.js
@@ -0,0 +1,142 @@
+// 1
+
+// function gridAsteriks() {
+//   for (let i = 0; i < 2; i++) {
+//     let ast = "";
+//     for (let j = 0; j < 2; j++) {
+//       ast += "*";
+//     }
+//     console.log(ast);
+//   }
+// }
+// gridAsteriks();
+
+// // 2
+
+// function gridNumbers(count) {
+//   for (let i = 1; i < 3; i++) {
+//     let row = "";
+//     for (let j = 0; j < 2; j++) {
+//       row += count + " ";
+//       count++;
+//     }
+//     console.log(row);
+//   }
+// }
+// gridNumbers(1);
+
+// // 3
+
+// let count = 1;
+// for (let i = 1; i < 5; i++) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row += count;
+//     count++;
+//   }
+//   console.log(row);
+// }
+
+// // 4
+
+// for (let i = 1; i < 10; i++) {
+//   let cell = i;
+//   let row = "";
+//   for (let j = 0; j < 10; j++) {
+//     row += cell + j + " ";
+//   }
+//   console.log(row);
+// }
+
+// // 5
+
+// for (let i = 1; i < 10; i++) {
+//   let row = "";
+//   for (let j = 1; j < 10; j++) {
+//     row += i * j + " ";
+//   }
+//   console.log(row);
+// }
+
+// 6;
+
+// for (let i = 0; i < 3; i++) {
+//   let row = "";
+//   for (let j = 0; j < 3; j++) {
+//     // let cell = "";
+//     if ((j === 0 || j === 2) && (i === 0 || i === 2)) {
+//       row += "  ";
+//     } else row += "* ";
+//   }
+//   console.log(row);
+// }
+
+// 7
+
+// count = 1;
+// for (let i = 1; i <= 3; i++) {
+//   let row = "";
+//   for (let j = 1; j <= 3; j++) {
+//     row += count + " ";
+//     count++;
+//   }
+//   console.log(row);
+// }
+
+// 8
+
+// count = 2;
+// for (let i = 1; i <= 3; i++) {
+//   let row = "";
+//   for (let j = 1; j <= 3; j++) {
+//     row += count + " ";
+//     count += 2;
+//   }
+//   console.log(row);
+// }
+
+// 9
+
+// for (let i = 1; i <= 3; i++) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += 1;
+//   }
+//   console.log(row);
+// }
+
+// 10
+
+// for (let i = 0; i < 4; i++) {
+//   let count = 0;
+//   let row = "";
+//   for (let j = 0; j < 4; j++) {
+//     if ((i % 2 === 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 === 0)) {
+//       row += 1 + " ";
+//     } /*if (i % 2 !== 1 || j % 2 !== 0) {
+//         }*/ else row += 0 + " ";
+//   }
+//   console.log(row);
+// }
+
+//  11
+
+// for (let i = 0; i < 3; i++) {
+//   let row = "";
+//   for (let j = 0; j < 3; j++) {
+//     row += j;
+//   }
+//   console.log(row);
+// }
+
+// 12
+
+// for (let i = 0; i < 3; i++) {
+//   let row = "";
+//   for (let j = 0; j < 3; j++) {
+//     row += i;
+//   }
+//   console.log(row);
+// }
+
+// 13
--- 25.7.24/homeWork/me/task9/main.js
+++ 25.7.24/homeWork/me/task9/main.js
@@ -26,10 +26,10 @@ console.log(extractsStrongPortion("bananas"));
 // 6
 
 function replaceInString(str) {
-  let replace = str.replace("blue", "red");
+  let replace = str.replace(/blue/g, "red");
   return replace;
 }
-console.log(replaceInString("blue car"));
+console.log(replaceInString("blue car blue"));
 
 // 7
 
@@ -76,6 +76,61 @@ function repeatString(str) {
   let repeat = str.repeat(3);
   return repeat;
 }
-console.log(repeatString("why????  "));
+console.log(repeatString("wiiiiiiiii  "));
 
 // 13
+
+function concat(str, strOne) {
+  let con = str.concat(strOne);
+  return con;
+}
+console.log(concat("hello", " world"));
+
+// 14
+
+function padString(str) {
+  let Pad = str.padStart(8, ",").padEnd(10, ".");
+  return Pad;
+}
+
+console.log(padString("hello"));
+
+// 15
+
+function axtractChar(str) {
+  let charN = str.slice(3, 4);
+  return charN;
+}
+console.log(axtractChar("dfsnjgk"));
+
+// 16
+
+function replacePartOfString(str) {
+  let replace = str.replace("blue", "red");
+  return replace;
+}
+console.log(replacePartOfString("blue car blue"));
+
+// 17
+
+function find(str) {
+  let check = str.includes("no", 5);
+  return check;
+}
+console.log(find("no hello world "));
+
+// 18
+
+function lestChar(str) {
+  let char = str[str.length - 1];
+  return char;
+}
+console.log(lestChar("hello world"));
+
+// 20
+
+function sliceMethod(str) {
+  let slice = str.slice(3);
+  return slice;
+}
+console.log(sliceMethod("nana banana"));
