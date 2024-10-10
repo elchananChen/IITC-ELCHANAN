a1e6e853810f66cc41eb18cf5fa78dea4ccc31b1
elchanan chen <elchananc79@gmail.com>
2024-10-10 19:40:58 +0300

    IconAPI

--- 2024/10/13API/main.js
+++ 2024/10/13API/main.js
@@ -1,30 +1,50 @@
+const input = document.querySelector("input");
 const div = document.querySelector("div");
 let webUrl = "github.com";
 
-function creatIcon() {
-  fetch(`https://icon.horse/icon/${webUrl}`)
-    .then((res) => res.blob()) // Change to .blob() to handle image data
-    .then((blob) => {
-      const img = document.createElement("img");
-      img.src = URL.createObjectURL(blob); // Create an object URL for the image
-      div.appendChild(img); // Append the image to the div
-    })
-    .catch((err) => console.log(`Error occurred: ${err}`));
+// ALLMOST UTILE
+async function reassignIcon() {
+  try {
+    const res = await fetch(`https://icon.horse/icon/${webUrl}`);
+    const blob = await res.blob();
+    document.querySelector("img").src = URL.createObjectURL(blob);
+  } catch (err) {
+    console.error(`not valid url: ${err}`);
+  }
 }
 
+// First interaction
+
+async function createIcon() {
+  try {
+    const res = await fetch(`https://icon.horse/icon/${webUrl}`);
+    const blob = await res.blob();
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(blob);
+    div.appendChild(img);
+  } catch (err) {
+    console.log(`Error occurred: ${err}`);
+  }
+}
+
+createIcon();
+
+// EVENT LISTENERS
 function reassignIconBySelect() {
   document.querySelector("#select").addEventListener("change", async (ev) => {
     webUrl = ev.target.value;
-    try {
-      const res = await fetch(`https://icon.horse/icon/${webUrl}`);
-      const blob = await res.blob();
-      document.querySelector("img").src = URL.createObjectURL(blob);
-      console.log("baba");
-    } catch (err) {
-      console.error(`not valid url: ${err}`);
-    }
+    reassignIcon();
     console.log("baba");
   });
 }
-creatIcon();
+
+function reassignIconByInput() {
+  document.querySelector(".change-btn").addEventListener("click", () => {
+    webUrl = input.value;
+    reassignIcon();
+    input.value = "";
+  });
+}
+
 reassignIconBySelect();
+reassignIconByInput();
